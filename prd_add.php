8f455aadc1ab45ec2ac698b03f46a220a99298ef
Dartaha <jatinrathi601@gmail.com>
2026-03-31 00:30:02 +0530

    Delete prd_add.php

--- prd_add.php
+++
@@ -1,7 +0,0 @@
-<?php
-$category = $_GET["category"];
-$prd_name = $_GET["prd_name"];
-$data = "$prd_name,$category\n";
-$file = fopen("prd.txt","a");
-fwrite($file,$data);
-header("location:/assingment/product%20page/Product.php");
