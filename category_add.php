fe649fe05ab0d17a21fcdfa228baf244253e65de
Dartaha <jatinrathi601@gmail.com>
2026-03-31 00:28:01 +0530

    Delete category_add.php

--- category_add.php
+++
@@ -1,7 +0,0 @@
-<?php
-$about = $_GET["about"];
-$category=$_GET["category"];
-$file=fopen("category.txt","a");
-$data="$category,$about\n";
-fwrite($file,$data);
-header("location:/assingment/product%20page/category.php");
